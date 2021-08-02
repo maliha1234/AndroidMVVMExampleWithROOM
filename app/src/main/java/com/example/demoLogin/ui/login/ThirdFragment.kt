b44308916d473e03fbdf9086d5301c1399a2e77a
Maliha Sarwat <mssarwat@amazon.com>
2021-08-02 13:11:03 -0500

    model added

--- app/src/main/java/com/example/demoLogin/ui/login/ThirdFragment.kt
+++ app/src/main/java/com/example/demoLogin/ui/login/ThirdFragment.kt
@@ -1,13 +1,18 @@
 package com.example.demoLogin.ui.login
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.myapplicationlogintest.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.myapplicationlogintest.databinding.ThirdFragmentBinding
+import com.example.myapplicationlogintest.R
+
 
 class ThirdFragment : Fragment() {
 
@@ -15,7 +20,7 @@ class ThirdFragment : Fragment() {
         fun newInstance() = ThirdFragment()
     }
 
-    private lateinit var viewModel: ThirdViewModel
+    private lateinit var loginViewModel: LoginViewModel
 
     private var _binding: ThirdFragmentBinding? = null
 
@@ -29,13 +34,76 @@ class ThirdFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = ThirdFragmentBinding.inflate(inflater, container, false)
+        val usernameEditText = binding.username
+        val passwordEditText = binding.password
+        val loginButton = binding.login
+        val showButton = binding.show
+        val loadingProgressBar = binding.loading
+
+        loginViewModel = ViewModelProvider(this, LoginViewModelFactory())
+            .get(LoginViewModel::class.java)
+        loginViewModel.getLoginFormState().observe(viewLifecycleOwner,
+            Observer<LoginFormState?> { loginFormState ->
+                if (loginFormState == null) {
+                    return@Observer
+                }
+                loginButton.isEnabled = loginFormState.isDataValid
+                if (loginFormState.usernameError != null) {
+                    usernameEditText.error = getString(loginFormState.usernameError!!)
+                }
+                if (loginFormState.passwordError != null) {
+                    passwordEditText.error = getString(loginFormState.passwordError!!)
+                }
+            })
+
+        loginViewModel.loginResult.observe(viewLifecycleOwner,
+            Observer { loginResult ->
+                if (loginResult == null) {
+                    return@Observer
+                }
+                loadingProgressBar.visibility = View.GONE
+                updateUiWithUser(loginResult.name)
+
+                //Complete and destroy login activity once successful
+                //finish();
+            })
+
+
+        val afterTextChangedListener: TextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // ignore
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                // ignore
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                loginViewModel.loginDataChanged(
+                    usernameEditText.text.toString(),
+                    passwordEditText.text.toString()
+                )
+            }
+        }
+
+        usernameEditText.addTextChangedListener(afterTextChangedListener)
+        passwordEditText.addTextChangedListener(afterTextChangedListener)
+
+        loginButton.setOnClickListener {
+            loadingProgressBar.visibility = View.VISIBLE
+            loginViewModel.login(
+                usernameEditText.text.toString(),
+                passwordEditText.text.toString()
+            )
+        }
 
         return binding.root
     }
 
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ThirdViewModel::class.java)
+
 
 
         // TODO: Use the ViewModel
@@ -44,6 +112,12 @@ class ThirdFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun updateUiWithUser(model: String) {
+        val welcome = getString(R.string.welcome) + model
+        // TODO : initiate successful logged in experience
+        Toast.makeText(activity, welcome, Toast.LENGTH_LONG).show()
     }
 
 }
