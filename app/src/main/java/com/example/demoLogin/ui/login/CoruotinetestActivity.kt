1f0e2b2d44e8f124c99471d7e0a41b88e977df25
Maliha Sarwat <mssarwat@amazon.com>
2021-08-03 10:00:18 -0500

    coroutine in global scope test

---
+++ app/src/main/java/com/example/demoLogin/ui/login/CoruotinetestActivity.kt
@@ -0,0 +1,36 @@
+package com.example.demoLogin.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import com.example.myapplicationlogintest.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class CoruotinetestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coruotinetest)
+
+       val mHandler = object : Handler(Looper.myLooper()!!){
+           override fun handleMessage(msg: Message) {
+               super.handleMessage(msg)
+
+           }
+       }
+
+        mHandler.obtainMessage()
+
+        GlobalScope.launch(Dispatchers.IO) { // not confined -- will work with main thread
+            Log.d("before", "Unconfined      : I'm working in thread ${Thread.currentThread().name}")
+            delay(500)
+            Log.d("after", "Unconfined      : After delay in thread ${Thread.currentThread().name}")
+        }
+
+    }
+}
